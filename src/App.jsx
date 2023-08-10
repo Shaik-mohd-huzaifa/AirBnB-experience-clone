05b5f478ad8fecb46dd50096f25d5b8d9e2a191e
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-08-10 23:06:22 +0530

    Add Hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Buttons } from "./elements/buttons";
 import { Navbar } from "./components/navbar";
+import { Hero } from "./components/Hero";
 const App = () => {
   return (
     <div className="">
       <Navbar />
+      <Hero />
       <h1>Hello World</h1>
       <Buttons type="submit" name="Sample" />
     </div>
