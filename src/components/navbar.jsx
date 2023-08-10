33d909daf460591df1f48ffa26a2566e8bf80159
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2023-08-10 20:55:29 +0530

    Added the Navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/airbnb-logo.png";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src={logo} alt="Airbnb Logo" />
+    </div>
+  );
+};
